77c9bc056081db6dcf7b5ad1861ec8068116845b
mitteth12 <mitten.ethan@gmail.com>
2021-09-12 18:36:45 -0500

    GW4 Update
    Another week, another top scoring performance. Within 15 points of the leader now.

---
+++ Python Files/FantasyPL2122_Heatmpa.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+train_fwd = pd.read_csv('/Users/ethanmitten/Desktop/Data Analytics/Fantasy PL Project/Correlation Heatmap Files/players_raw_currentFWD.csv')
+train_def = pd.read_csv('/Users/ethanmitten/Desktop/Data Analytics/Fantasy PL Project/Correlation Heatmap Files/players_raw_currentDEF.csv')
+train_mid = pd.read_csv('/Users/ethanmitten/Desktop/Data Analytics/Fantasy PL Project/Correlation Heatmap Files/players_raw_currentMID.csv')
+train_gk = pd.read_csv('/Users/ethanmitten/Desktop/Data Analytics/Fantasy PL Project/Correlation Heatmap Files/players_raw_currentGK.csv')
+
+f, ax = plt.subplots(figsize=(14,14))
+sns.heatmap(train_fwd.corr(), linewidth=.5)
+
+f, ax = plt.subplots(figsize=(14,14))
+sns.heatmap(train_def.corr(), linewidth=.5)
+
+f, ax = plt.subplots(figsize=(14,14))
+sns.heatmap(train_gk.corr(), linewidth=.5)
+
+f, ax = plt.subplots(figsize=(14,14))
+sns.heatmap(train_mid.corr(), linewidth=.5)
+
